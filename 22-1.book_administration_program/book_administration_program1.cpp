8f3ff9c09ca581e02df07463fa4c1dfaba533a7a
JIKANG <jikang0105@naver.com>
2020-06-06 22:39:05 +0900

    20200606(1)

--- 22-1.book_administration_program/book_administration_program1.cpp
+++ 22-1.book_administration_program/book_administration_program1.cpp
@@ -26,7 +26,7 @@ struct _tagBook {
 	char	strName[NAME_SIZE];
 	char	strPrise[PRISE_SIZE];
 	char	strNumber[NUMBER_SIZE];
-	int		iBCount;
+	int		iBCount; 
 	int		iBsCount;
 	// 대여불가 0(true), 대여가능 >= 1(false)
 	int		iRent;
@@ -48,7 +48,11 @@ int main() {
 	// 단일 책의 개수.
 	int iBookCount = 0;
 	// 책의 종류(중복되는 책은 세지 않는다.)
-	int iBooksCount = 0;
+	int iBooksCount = 1;
+
+	char strName[NAME_SIZE] = {};
+
+	int iRBookCount = 0;
 
 	while (true) {
 
@@ -80,41 +84,73 @@ int main() {
 
 			cout << "책 이름 : ";
 			cin.getline(tBookArr[iBookIdx].strName, NAME_SIZE);
-			cin.ignore(1024, '\t');
+			cin.ignore(1024, '\n');
 
 			cout << "대여 금액 : ";
 			cin.getline(tBookArr[iBookIdx].strPrise, PRISE_SIZE);
-			cin.ignore(1024, '\t');
+			cin.ignore(1024, '\n');
 
 			cout << "책 번호 : ";
 			cin.getline(tBookArr[iBookIdx].strNumber, NUMBER_SIZE);
-			cin.ignore(1024, '\t');
+			cin.ignore(1024, '\n');
 
 			cout << "권 수 : ";
 			cin >> iBookCount;
 				
 			tBookArr[iBookIdx].iBCount = iBookCount;
 
-			// 서 bRent값에 0을 부여하여 대여 가능한 상태로 만든다.
+			// 서 iRent값에 권 수를 대입하고 0이되면 대여 불가능한 상태로 만든다.
 			tBookArr[iBookIdx].iRent = iBookCount;
 
-			tBookArr[iBookIdx].iBsCount += iBookCount;
+			tBookArr[iBookIdx].iBsCount += iBooksCount;
 
 			++iBooksCount;
-			++iBookNumber;
-
+			
+			cout << "책 추가 완료" << endl;
 
 			break;
 		case MENU_RENT:
 			break;
 		case MENU_RETURN:
+			system("cls");
+			cout << "=============== 책 반납 ===============" << endl;
+			cin.ignore(1024, '\n');
+			cout << "반납할 책을 입력하세요 : ";
+			cin.getline(strName, NAME_SIZE);
+			cin.ignore(1024, '\n');
+			
+
+			for (int i = 0; i < iBooksCount; ++i) {
+				if(strcmp(tBookArr[i].strName, strName) == 0){
+					cout << "반납할 권 수를 입력하세요 : ";
+					cin >> iRBookCount;
+					tBookArr[i].iBCount += iRBookCount;
+					cout << tBookArr[i].strName << iRBookCount << "권이 반납되었습니다." << endl;
+					break;
+				}
+
+			}
+
 			break;
 		case MENU_LIST:
 			system("cls");
-			for(int i = 0; i <)
+			cout << "=============== 책 목록 ===============" << endl;
+			for (int i = 0; i <= iBookIdx; ++i) {
+				cout << "책 이름 : " << tBookArr[i].strName << endl;
+				cout << "책 가격 : " << tBookArr[i].strPrise << endl;
+				cout << "책 번호 : " << tBookArr[i].strNumber << endl;
+				cout << "권 수 : " << tBookArr[i].iBCount << endl;
+				if (tBookArr[iBookIdx].iBCount > 0) {
+					cout << "대여 가능" << endl;
+				}
+				else {
+					cout << "대여 불가" << endl;
+				}
+			}
 			break;
 		case MENU_END:
 			break;
 		}
+		system("pause");
 	}
 }
